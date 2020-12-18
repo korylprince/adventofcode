3064e6453f0b0708a15baca38e3af604bafe52a7
Kory Prince <korylprince@gmail.com>
2020-12-18 17:25:32 -0600

    finished 2020/18

--- 2020/18/main.py
+++ 2020/18/main.py
@@ -1,54 +1,100 @@
+import numpy as np
+
 MULT = "*"
 PLUS = "+"
 OPEN = "("
 CLOSE = ")"
 
-def tokenize(expr):
-    tokens = []
-    inter = ""
-    for c in expr:
-        if c in (MULT, PLUS, OPEN, CLOSE):
-            if len(inter) > 0:
-                tokens.append(int(inter))
-                inter = ""
-            tokens.append(c)
-        elif c == " ":
-            pass
-        else:
-            inter += c
-    if len(inter) > 0:
-        tokens.append(int(inter))
-    return tokens
+class Parser:
+    def __init__(self, expr):
+        self.expr = expr
+        self.pos = 0
 
-def evaluate(tokens):
-    val = 0
-    op = None
-    pos = 0
-    while pos < len(tokens):
-        if (t := tokens[pos]) in (MULT, PLUS):
-            op = t
-        elif isinstance(t, int):
-            if op is None:
-                val = t
-            elif op == MULT:
-                val *= t
-            elif op == PLUS:
-                val += t
-        elif t == OPEN:
-            v, p = evaluate(tokens[pos + 1:])
-            if op is None:
-                val = v
-            elif op == MULT:
-                val *= v
-            elif op == PLUS:
-                val += v
-            pos += p + 1
-        elif t == CLOSE:
-            return val, pos
-        pos += 1
-    return val
+    @property
+    def tok(self):
+        return self.expr[self.pos]
+
+    def canPeek(self):
+        return self.pos < len(self.expr)
+
+    def parseWhitespace(self):
+        while self.canPeek():
+            if self.tok == " ":
+                self.pos += 1
+            else:
+                break
+
+    def parseExpression(self):
+        return self.parseOps()
+
+    def parseOps(self):
+        val = self.parseParens(self.parseExpression)
+        while self.canPeek():
+            self.parseWhitespace()
+            if self.tok == PLUS:
+                self.pos += 1
+                val += self.parseParens(self.parseExpression)
+            elif self.tok == MULT:
+                self.pos += 1
+                val *= self.parseParens(self.parseExpression)
+            else:
+                break
+        return val
+
+    def parseAdvancedExpression(self):
+        return self.parseMultiplication()
+
+    def parseMultiplication(self):
+        values = [self.parseAddition()]
+        while self.canPeek():
+            self.parseWhitespace()
+            if self.tok == MULT:
+                self.pos += 1
+                values.append(self.parseAddition())
+            else:
+                break
+        return np.product(values)
+
+    def parseAddition(self):
+        values = [self.parseParens(self.parseAdvancedExpression)]
+        while self.canPeek():
+            self.parseWhitespace()
+            if self.tok == PLUS:
+                self.pos += 1
+                values.append(self.parseParens(self.parseAdvancedExpression))
+            else:
+                break
+        return sum(values)
+
+    def parseParens(self, exprParser):
+        self.parseWhitespace()
+        if self.tok == OPEN:
+            self.pos += 1
+            val = exprParser()
+            self.parseWhitespace()
+            if self.tok != CLOSE:
+                raise Exception(f"Expected CLOSE at pos {self.pos}")
+            self.pos += 1
+            return val
+        else:
+            return self.parseInt()
+
+    def parseInt(self):
+        # yes, I know I only need to parse single-digit ints
+        val = ""
+        while self.canPeek():
+            self.parseWhitespace()
+            if (tok := self.tok).isnumeric():
+                self.pos += 1
+                val += tok
+            elif tok in (CLOSE, MULT, PLUS):
+                return int(val)
+            else:
+                raise Exception(f"Unknown token {tok} at pos {self.pos}")
+        return int(val)
 
 with open("./input.txt") as f:
-    exprs = [tokenize(line.strip()) for line in f.read().strip().splitlines()]
+    exprs = [line.strip() for line in f.read().strip().splitlines()]
 
-print("Answer 1:", sum([evaluate(expr) for expr in exprs]))
+print("Answer 1:", sum([Parser(expr).parseExpression() for expr in exprs]))
+print("Answer 2:", sum([Parser(expr).parseAdvancedExpression() for expr in exprs]))
