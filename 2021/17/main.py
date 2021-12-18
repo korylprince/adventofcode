5b04c17921d6cf49331fbe48ed0557e94adb823c
Kory Prince <korylprince@gmail.com>
2021-12-17 21:08:57 -0600

    optimized 2021/17

--- 2021/17/main.py
+++ 2021/17/main.py
@@ -1,20 +1,61 @@
 import re
+from functools import cache
 
 with open("./input.txt") as f:
     ints = [int(i) for i in re.findall(r"\-?\d+", f.read())]
     xlow, xhigh, ylow, yhigh = ints[0], ints[1], ints[2], ints[3]
 
-def step(dx, dy, t):
+@cache
+def stepx(dx, t):
     tw = t if dx > t else t - (t - dx)
-    return (
-        int((dx * (dx + 1) - (dx-tw) * ((dx-tw) + 1))/2), # x
-        int((dy * (dy + 1) - (dy-t) * ((dy-t) + 1))/2), # y
-        int((dy * (dy + 1))/2), # highest
-    )
+    return int((dx * (dx + 1) - (dx-tw) * ((dx-tw) + 1))/2)
+
+@cache
+def stepy(dy, t):
+    return int((dy * (dy + 1) - (dy-t) * ((dy-t) + 1))/2)
+
+@cache
+def highest(dy, t):
+    return int((dy * (dy + 1))/2)
+
+@cache
+def binx(dx, t):
+    tgt = xhigh + 1
+    low = 0
+    high = t
+    mid = None
+    while low + 1 < high:
+        mid = (high + low) // 2
+        x = stepx(dx, mid)
+        if x < tgt:
+            low = mid
+        elif x > tgt:
+            high = mid
+        else:
+            return high
+    return high
+
+@cache
+def biny(dy, t1, t2):
+    tgt = yhigh + 1
+    low = t1
+    high = t2
+    mid = None
+    while low < high - 1:
+        mid = (high + low) // 2
+        y = stepy(dy, mid)
+        if y > tgt:
+            low = mid
+        elif y < tgt:
+            high = mid
+        else:
+            return low
+    return low
+
 
 def find_intersect(dx, dy):
     t = dx
-    x, y, h = step(dx, dy, t)
+    x = stepx(dx, t)
 
     # never reaches zone x
     if x < xlow:
@@ -22,28 +63,37 @@ def find_intersect(dx, dy):
 
     # ends vertically in zone - track forward in time
     if x <= xhigh:
-        t1 = t
+        t1 = biny(dy, t, t*2)
+        y = stepy(dy, t1)
         while y >= ylow:
             if y <= yhigh:
-                return x, y, h
+                return x, y, highest(dy, t1)
             t1 += 1
-            x, y, h = step(dx, dy, t1)
+            y = stepy(dy, t1)
 
     # goes past zone - track backward in time
+    t = binx(dx, t)
     while x >= xlow:
         if x <= xhigh:
+            y = stepy(dy, t)
             if ylow <= y <= yhigh:
-                return x, y, h
+                return x, y, highest(dy, t)
         t -= 1
-        x, y, h = step(dx, dy, t)
+        x = stepx(dx, t)
+
+
+# find minimum x
+xstart = 1
+while stepx(xstart, xstart) < xlow:
+    xstart += 1
 
 
 m = (0, 0, 0)
 count = 0
 found = set()
-# x and y ranges found with experimentation
-for x in range(0, 300):
-    for y in range(ylow-1, 100):
+
+for x in range(xstart, xhigh + 2):
+    for y in range(ylow-1, 100): # y max found through experimentation
         res = find_intersect(x, y)
         if res is not None:
             count += 1
