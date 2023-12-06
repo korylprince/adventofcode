fb69d9f08a8644648a3721e0f411863c8bf81a35
Kory Prince <korylprince@gmail.com>
2023-12-05 23:38:13 -0800

    added quadratic formula method to 2023/06

--- 2023/06/main.py
+++ 2023/06/main.py
@@ -31,13 +31,25 @@ def find_roots(total, goal):
             e = c
     return b, d
 
+def find_roots_quad(total, goal):
+    #- hold ^ 2 + total * hold - goal = 0
+    x1 = (-total + math.sqrt(total**2 - 4*-1*(-goal))) / (2 * -1)
+    x2 = (-total - math.sqrt(total**2 - 4*-1*(-goal))) / (2 * -1)
+    return int(x1), int(x2)
+
 def wins(total, goal):
     r1, r2 = find_roots(total, goal)
     return r2 - r1 + 1
 
+def wins_quad(total, goal):
+    r1, r2 = find_roots_quad(total, goal)
+    return r2 - r1
+
 print("Answer 1:", math.prod([wins(total, goal) for total, goal in map]))
+print("Answer 1 quadratic:", math.prod([wins_quad(total, goal) for total, goal in map]))
 
 total = int("".join(re.findall(r"\d", lines[0])))
 goal = int("".join(re.findall(r"\d", lines[1])))
 
 print("Answer 2:", wins(total, goal))
+print("Answer 2 quadratic:", wins_quad(total, goal))
